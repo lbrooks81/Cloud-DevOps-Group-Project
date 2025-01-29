53757848b5458ef8790392d0555fe860b0e1ca1a
msallese22 <msallese01@southhills.edu>
2025-01-28 19:40:43 -0500

    trying to add navbar, not showing up
    will troubleshoot more later

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgOptimizedImage
+  ],
   templateUrl: './header.component.html',
+  standalone: true,
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
