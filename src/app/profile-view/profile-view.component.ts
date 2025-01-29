6a4a2ef1996dbf0cb051080814ff5f35652385d0
kcraycraft45 <kcraycraft45@southhills.edu>
2025-01-28 20:36:15 -0500

    Logo Prototype positioned in profile view...No Touching

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {LogoComponent} from '../components/logo/logo.component';
 
 @Component({
   selector: 'app-profile-view',
-  imports: [],
+  imports: [
+    LogoComponent
+  ],
   templateUrl: './profile-view.component.html',
+  standalone: true,
   styleUrl: './profile-view.component.css'
 })
 export class ProfileViewComponent {
