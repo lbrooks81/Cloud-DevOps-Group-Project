cf6744d4fc6de79d5c9591337f965f35438f28a3
kcraycraft45 <kcraycraft45@southhills.edu>
2025-01-30 19:44:03 -0500

    hilo

--- src/app/database-view/database-view.component.ts
+++ src/app/database-view/database-view.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import {LogoComponent} from '../components/logo/logo.component';
 import {DatabaseTableComponent} from '../components/database-table/database-table.component';
+import {HeaderComponent} from '../components/header/header.component';
 
 @Component({
   selector: 'app-database-view',
   imports: [
-    LogoComponent,
-    DatabaseTableComponent
+    DatabaseTableComponent,
+    HeaderComponent
   ],
   templateUrl: './database-view.component.html',
   standalone: true,
