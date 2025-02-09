806d503cf9ad406a06a7a116d434a1805b757733
rhenninger61 <rhenninger61@southhills.edu>
2025-02-09 15:55:51 -0500

    Database credentials added

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,19 +1,30 @@
+import {Employee} from './entities/employee';
+
 export { ServerData };
 
 import {DataSource} from 'typeorm';
+import {Department} from './entities/department';
+import {Plant} from './entities/plant';
+import {MicroComponent} from './entities/micro-component';
+import {Company} from './entities/company';
+import {Orders} from './entities/orders';
+import {PermissionLevel} from './entities/permission-level';
+import {Roles} from './entities/roles';
+import {Vendor} from './entities/vendor';
+import {Part} from './entities/part';
+import {ManufacturedPart} from './entities/manufactured-part';
 
 //Server Data Source
 const ServerData = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: "it239-devops.database.windows.net",
   port: 3306,
-  //username: ''
-  //password: ''
-  //database: ''
+  username: 'IT239',
+  password: 'Devops239!@',
+  database: 'it239-devops.database.windows.net',
   synchronize: false,
   logging: true,
-  //TODO: Get the database .sql file to run locally for testing
-  //entities: [Employee, Department, Plant, MicroComponents, Company, Orders, PermissionLevel, Roles, Vendor, Part, ManufacturedPart]
+  entities: [Employee, Department, Plant, MicroComponent, Company, Orders, PermissionLevel, Roles, Vendor, Part, ManufacturedPart],
   subscribers: [],
   migrations: []
 });
