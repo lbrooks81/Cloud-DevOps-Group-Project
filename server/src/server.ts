93d0ae993a5079e7dbd1b78f7eae351799bbb00f
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-02-27 17:39:01 -0500

    Fixed an issue where CORS requests would not run
    This seemed to only affect MY LAPTOP SPECIFICALLY >:(

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,7 +31,13 @@ https.createServer(options, app).listen(port, () => {
 app.use(cors());
 
 app.use(bodyParser.json()); // take a request body and turn it into a json object
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 // Initialize my data source
 ServerData.initialize()
