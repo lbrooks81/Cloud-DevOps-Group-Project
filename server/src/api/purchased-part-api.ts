058bf72fdcfd9f04cd15e362b56e7bdb6992da33
forma-cristata <kaaucri@gmail.com>
2025-03-05 20:03:16 -0500

    Try Catch - purchased-part-api.ts

--- server/src/api/purchased-part-api.ts
+++ server/src/api/purchased-part-api.ts
@@ -6,127 +6,193 @@ import {Employee} from '../entities/employee';
 
 async function purchasedPartRequests() {
     // get all purchased parts
+    // BUTTS
     app.get('/purchased-parts', async (req, res) => {
-        const purchasedParts = await ServerData.getRepository(PurchasedPart).find();
-        res.json(purchasedParts);
+        try {
+            const purchasedParts = await ServerData.getRepository(PurchasedPart).find();
+            res.json(purchasedParts);
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error fetching all purchased parts"
+            });
+        }
     });
 
     // get purchased part by composite key {partID, plantID}
+    // BUTTS
     app.get('/purchased-parts/:partID/:plantID', async (req, res) => {
-        const partID = Number(req.params.partID);
-        const plantID = Number(req.params.plantID);
+        try {
+            const partID = Number(req.params.partID);
+            const plantID = Number(req.params.plantID);
 
-        const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
-            partID: partID,
-            plantID: plantID
-        });
-
-        if (!purchasedPart) {
-            res.status(404).json({
-                message: `Purchased Part with Part ID ${partID} and Plant ID ${plantID} not found`
+            const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
+                partID: partID,
+                plantID: plantID
             });
-        } else {
-            res.json(purchasedPart);
+
+            if (!purchasedPart) {
+                res.status(404).json({
+                    message: `Purchased Part with Part ID ${partID} and Plant ID ${plantID} not found`
+                });
+            } else {
+                res.json(purchasedPart);
+            }
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error fetching purchased part by ID"
+            });
         }
     });
 
     // update a specific purchased part based on an id
+    // BUTTS
     app.put('/purchased-parts/:partID/:plantID', async (req, res) => {
-        const partID = Number(req.params.partID);
-        const plantID = Number(req.params.plantID);
-        const purchasedPartData = req.body;
+        try {
+            const partID = Number(req.params.partID);
+            const plantID = Number(req.params.plantID);
+            const purchasedPartData = req.body;
 
-        const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
-        const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
-            partID: partID,
-            plantID: plantID
-        });
-
-        if (!purchasedPart) {
-            res.status(404).json({
-                message: `Purchased Part with partID ${partID} and plantID ${plantID} not found`
+            const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
+            const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
+                partID: partID,
+                plantID: plantID
             });
-        } else {
-            purchasedPart.plantID = purchasedPartData.plantID;
-            purchasedPart.partID = purchasedPartData.partID;
-            purchasedPart.date = purchasedPartData.date;
 
-            await purchasedPartRepository.save(purchasedPart);
-            res.json(purchasedPart);
+            if (!purchasedPart) {
+                res.status(404).json({
+                    message: `Purchased Part with partID ${partID} and plantID ${plantID} not found`
+                });
+            } else {
+                purchasedPart.plantID = purchasedPartData.plantID;
+                purchasedPart.partID = purchasedPartData.partID;
+                purchasedPart.date = purchasedPartData.date;
+
+                await purchasedPartRepository.save(purchasedPart);
+                res.json(purchasedPart);
+            }
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error updating purchased part by ID"
+            });
         }
     });
 
     // delete a specific purchased part based on an id
+    // BUTTS
     app.delete('/purchased-parts/:partID/:plantID', async (req, res) => {
-        const partID = Number(req.params.partID);
-        const plantID = Number(req.params.plantID);
+        try {
+            const partID = Number(req.params.partID);
+            const plantID = Number(req.params.plantID);
 
-        const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
-        const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
-            partID: partID,
-            plantID: plantID
-        });
-
-        if (!purchasedPart) {
-            res.status(404).json({
-                message: `Purchased Part with partID ${partID} and plantID ${plantID} not found`
+            const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
+            const purchasedPart = await ServerData.getRepository(PurchasedPart).findOneBy({
+                partID: partID,
+                plantID: plantID
             });
-        } else {
-            await purchasedPartRepository.delete(purchasedPart);
-            res.json({
-                message: `Purchased Part with partID ${partID} and plantID ${plantID} has been deleted`
+
+            if (!purchasedPart) {
+                res.status(404).json({
+                    message: `Purchased Part with partID ${partID} and plantID ${plantID} not found`
+                });
+            } else {
+                await purchasedPartRepository.delete(purchasedPart);
+                res.json({
+                    message: `Purchased Part with partID ${partID} and plantID ${plantID} has been deleted`
+                });
+            }
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error deleting purchased part by ID"
             });
         }
     });
 
     // create a new purchased part
+    // BUTTS
     app.post('/purchased-parts', async (req, res) => {
-        const purchasedPartData = req.body;
+        try {
+            const purchasedPartData = req.body;
 
-        const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
-        const newPurchasedPart = purchasedPartRepository.create({
-            plantID: purchasedPartData.plantID,
-            partID: purchasedPartData.partID,
-            date: purchasedPartData.date
-        });
+            const purchasedPartRepository = ServerData.getRepository(PurchasedPart);
+            const newPurchasedPart = purchasedPartRepository.create({
+                plantID: purchasedPartData.plantID,
+                partID: purchasedPartData.partID,
+                date: purchasedPartData.date
+            });
 
-        await purchasedPartRepository.save(newPurchasedPart);
-        res.json(newPurchasedPart);
+            await purchasedPartRepository.save(newPurchasedPart);
+            res.json(newPurchasedPart);
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error creating purchased part"
+            });
+        }
     });
 
     // Employees can only view purchased parts from their plant - DONE
+    // BUTTS
     app.get('/our-purchased-parts/:id', async (req, res) => {
-        // GET EMPLOYEE BY ID
-        const id = Number(req.params.id);
-        let plantIdentification: number | undefined;
+        try {
+            // GET EMPLOYEE BY ID
+            const id = Number(req.params.id);
+            let plantIdentification: number | undefined;
+            let found = true;
 
-        const employee: Employee | null = await ServerData.getRepository(Employee).findOneBy({
-            employeeID: id
-        });
-
-        if (!employee) {
-            res.status(404).json({
-                message: `Employee with ID ${id} not found`
+            const employee: Employee | null = await ServerData.getRepository(Employee).findOneBy({
+                employeeID: id
             });
-        } else {
-            plantIdentification = employee.plantID;
-            /*res.json(plantIdentification);*/
-        }
+            let message;
 
-        // GET PURCHASED PARTS BY PLANT ID
-        const purPart: PurchasedPart[] | null = await ServerData.getRepository(PurchasedPart).find({
-            where: {
-                plantID: plantIdentification
+            if (!employee) {
+                found = false;
+                message = `Employee with ID ${id} not found`;
+
+/*                res.status(404).json({
+                    message: message
+                });*/
+            } else {
+                plantIdentification = employee.plantID;
+
+                // GET PURCHASED PARTS BY PLANT ID
+                const purPart: PurchasedPart[] | null = await ServerData.getRepository(PurchasedPart).find({
+                    where: {
+                        plantID: plantIdentification
+                    }
+                });
+
+                if (!purPart) {
+                    found = false;
+                    message = `Purchased Parts with Plant ID ${plantIdentification} not found`
+                    /*                res.status(404).json({
+                                        message: `Purchased Parts with Plant ID ${plantIdentification} not found`
+                                    });*/
+                } else {
+                    message = purPart;
+                    /*res.json(purPart);*/
+                }
             }
-        });
 
-        if (!purPart) {
-            res.status(404).json({
-                message: `Purchased Parts with Plant ID ${plantIdentification} not found`
+            if (found) {
+                res.json(message);
+            } else {
+                res.status(404).json({
+                    message: message
+                });
+            }
+
+
+
+
+        }
+        catch (e) {
+            res.status(500).json({
+                message: "Error fetching purchased parts by plant ID"
             });
-        } else {
-            res.json(purPart);
         }
     });
-
 }
